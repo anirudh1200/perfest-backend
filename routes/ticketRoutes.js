18864e42f7a0ba415fb30ec1865628d95c73a066
someshkoli <kolisomesh27@gmail.com>
2019-09-04 16:52:16 +0530

    ticket issue to mail

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -3,12 +3,18 @@ const middleware = require('../app/middleware/middleware');
 
 const ticket = require("../app/controllers/ticketController");
 
-router.post("/issue", middleware.authAdminVol,ticket.issue);
+// @route		POST /ticket/issue
+// @desc		ticket will be issued
+// @params	    name, email, phone, event_id, price, paid, participantNo 
+// @return	    siccess(true/false), error(if any)
+router.post("/issue", ticket.issue);
+// middleware.authAdminVol,
+
 
 // @route		POST /ticket/invalidate
 // @desc		ticket will no longer be valid
-// @params	ticketId
-// @return	siccess(true/false), error(if any)
+// @params	    ticketId
+// @return	    success(true/false), error(if any)
 router.post('/invalidate', middleware.authAdminVol, ticket.invalidate);
 
 //TODO event scan
--- test.sh
+++
@@ -1,3 +0,0 @@
-SOMESH="someshkoli"
-
-echo $SOMESH
