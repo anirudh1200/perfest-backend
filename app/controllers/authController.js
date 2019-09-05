2a1ede48df2a3734de14013428b2ac88232b3266
= <anirudh.b.123@gmail.com>
2019-09-05 15:03:20 +0530

    tweaked login route and added bcrypt

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -3,70 +3,10 @@ const User = require('../database/models/user');
 const Volunteer = require('../database/models/volunteer');
 const Admin = require('../database/models/admin');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
-exports.login = async (req, res) => {
-    let user = null;
-    let type = null;
-    let token = '';
-    try {
-        if (req.body.phone) {
-            user = await User.findOne({ 'contact.phone': req.body.phone });
-        } else {
-            user = await User.findOne({ 'contact.email': req.body.email });
-        }
-        if (user) {
-            if (user.password === req.body.password) {
-                type = 'user';
-            } else {
-                res.status(401).json({ success: false, token, error: 'invalid credentials' });
-                return;
-            }
-        } else {
-            try {
-                if (req.body.phone) {
-                    user = await Volunteer.findOne({ 'contact.phone': req.body.phone });
-                } else {
-                    user = await Volunteer.findOne({ 'contact.email': req.body.email });
-                }
-                if (user) {
-                    if (user.password === req.body.password) {
-                        type = 'volunteer';
-                    } else {
-                        res.status(401).json({ success: false, token, error: 'invalid credentials' });
-                        return;
-                    }
-                } else {
-                    try {
-                        if (req.body.phone) {
-                            user = await Admin.findOne({ 'contact.phone': req.body.phone });
-                        } else {
-                            user = await Admin.findOne({ 'contact.email': req.body.email });
-                        }
-                        if (user) {
-                            if (user.password === req.body.password) {
-                                type = 'admin';
-                            } else {
-                                res.status(401).json({ success: false, token, error: 'invalid credentials' });
-                                return;
-                            }
-                        } else {
-                            res.status(401).json({ success: false, token, error: 'no user found' });
-                            return;
-                        }
-                    } catch (err) {
-                        res.json({ success: false, token, error: toString(err) });
-                        return;
-                    }
-                }
-            } catch (err) {
-                res.json({ success: false, token, error: toString(err) });
-                return;
-            }
-        }
-    } catch (err) {
-        res.json({ success: false, token, error: toString(err) });
-        return;
-    }
+const sendToken = (res, user, type) => {
     let jwt_token = jwt.sign({
         type,
         userId: user._id,
@@ -74,6 +14,48 @@ exports.login = async (req, res) => {
             expiresIn: "1d",
         });
     res.json({ success: true, token: jwt_token });
+}
+
+const checkUser = (req, res, user, token, error) => {
+    if (user) {
+        if (user.password === req.body.password) {
+            let type = 'user';
+            sendToken(res, user, type);
+        } else {
+            res.status(401).json({ success: false, token, error: 'invalid credentials' });
+        }
+        return true;
+    }
+}
+
+exports.login = async (req, res) => {
+    let token = '';
+    let error = false;
+    let user = await User.findOne({ 'contact.email': req.body.email })
+        .catch(err => {
+            res.json({ success: false, token, error: err });
+            error = true;
+        });
+    if (error || checkUser(req, res, user, token)) {
+        return;
+    }
+    user = await Volunteer.findOne({ 'contact.email': req.body.email })
+        .catch(err => {
+            res.json({ success: false, token, error: err });
+            error = true;
+        });
+    if (error || checkUser(req, res, user, token)) {
+        return;
+    }
+    user = await Admin.findOne({ 'contact.email': req.body.email })
+        .catch(err => {
+            res.json({ success: false, token, error: err });
+            error = true;
+        });
+    if (error || checkUser(req, res, user, token)) {
+        return;
+    }
+    res.status(401).json({ success: false, token, error: 'no user found' });
 }
 
 exports.signup = async (req, res, next) => {
@@ -92,14 +74,17 @@ exports.signup = async (req, res, next) => {
         type: false,
         csi_member: req.body.member
     };
-    let newUser = new User(data);
-    try {
-        await newUser.save();
-    } catch (err) {
-        res.json({ success: false, error: toString(err) });
-        return;
-    }
-    res.json({ success: true });
+    bcrypt.hash(data.password, saltRounds, async (err, hash) => {
+        let newUser = new User(data);
+        try {
+            await newUser.save();
+        } catch (err) {
+            res.json({ success: false, error: err });
+            return;
+        }
+        res.json({ success: true });
+    });
+
 }
 
 exports.createanonymous = async (req, res) => {
@@ -107,27 +92,31 @@ exports.createanonymous = async (req, res) => {
     let phone = req.body.phone;
     let email = req.body.email;
     let password = req.body.password;
-    if (req.body.phone) {
-        data = {
-            contact: {
-                phone
-            },
-            password
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (!err) {
+            if (req.body.phone) {
+                data = {
+                    contact: {
+                        phone
+                    },
+                    password: hash
+                }
+            } else {
+                data = {
+                    contact: {
+                        email
+                    },
+                    password: hash
+                }
+            }
+            let newUser = new User(data);
+            try {
+                await newUser.save();
+            } catch (err) {
+                res.json({ success: false, error: err });
+                return;
+            }
+            res.json({ success: true });
         }
-    } else {
-        data = {
-            contact: {
-                email
-            },
-            password
-        }
-    }
-    let newUser = new User(data);
-    try {
-        await newUser.save();
-    } catch (err) {
-        res.json({ success: false, error: toString(err) });
-        return;
-    }
-    res.json({ success: true });
+    });
 }
