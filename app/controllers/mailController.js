68447b9f4b9e55e527e8e6357c456c2971c2567b
someshkoli <kolisomesh27@gmail.com>
2019-09-08 02:20:15 +0530

    controllers: mailcontroller |  mail service provider changed from mailgun to sendgrid

--- app/controllers/mailController.js
+++ app/controllers/mailController.js
@@ -1,12 +1,15 @@
 const User = require('../database/models/user'),
     ticket = require('../database/models/ticket')
-mailgun = require('mailgun-js');
+mailgun = require('mailgun.js');
+sendgrid = require('@sendgrid/mail');
+
+
 require('dotenv').config();
 //MAILGUN CONFIGURATION
-const mail = new mailgun({
-    apiKey: process.env.MAILGUN_API,
-    domain: process.env.MAIL_DOMAIN
-});
+// const mail=new mailgun()
+
+//SENDGRID CONFIGURATION
+const mail = new sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 //add controllers below
 exports.eventConfirmation = async (user, Ticket) => {
     let userId = user._id;
@@ -16,22 +19,28 @@ exports.eventConfirmation = async (user, Ticket) => {
     let generated_link = process.env.HOST + "/t/" + genString;
     var data = {
         from: 'Somesh Koli <kolisomesh27@gmail.com>',
-        // to:  'Somesh Koli <kolisomesh27@gmail.com>',
-        to: userEmail,
+        to:  'Somesh Koli <kolisomesh27@gmail.com>',
+        // to: userEmail,
         subject: 'Hello',
         text: generated_link
     }
     //to be updated in databse
     await ticket.findByIdAndUpdate(Ticket._id, { url: genString }, (err) => { console.log });
     // console.log(generated_link)
-    mail.messages().send(data, (error, body) => {
-        if(error){
-            console.log(error, body);
-            return false;
-        } else {
-            return true;
-        }
-    });
+    //MAILING VIA MAILGUN
+    // await mail.messages().send(data, (error, body) => {
+    //     console.log(error, body)
+    // })
+    //MAILING VIA SENDGR
+    try {
+        await mail.send(data);
+    }
+    catch(err){
+        console.log(err);
+        return false;
+    }
+
+    return true;
 }
 
 //mail function
