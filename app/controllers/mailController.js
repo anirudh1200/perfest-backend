5ecd3924f6e47ec026761307e282a9633338c1cd
Neil Agarwal <neil@neilagarwal.me>
2019-09-08 03:21:50 +0530

    mailController: Set up transactional mailing using Sendgrid

--- app/controllers/mailController.js
+++ app/controllers/mailController.js
@@ -1,15 +1,13 @@
 const User = require('../database/models/user'),
     ticket = require('../database/models/ticket')
-mailgun = require('mailgun.js');
 sendgrid = require('@sendgrid/mail');
 
 
 require('dotenv').config();
-//MAILGUN CONFIGURATION
-// const mail=new mailgun()
 
 //SENDGRID CONFIGURATION
-const mail = new sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
 //add controllers below
 exports.eventConfirmation = async (user, Ticket) => {
     let userId = user._id;
@@ -18,28 +16,22 @@ exports.eventConfirmation = async (user, Ticket) => {
     let genString = Math.random().toString(36).substring(5);
     let generated_link = process.env.HOST + "/t/" + genString;
     var data = {
-        from: 'Somesh Koli <kolisomesh27@gmail.com>',
-        to:  'Somesh Koli <kolisomesh27@gmail.com>',
-        // to: userEmail,
-        subject: 'Hello',
+        from: 'Perfest <tickets@perfest.co>',
+        to: userEmail,
+        subject: 'Your Tickets',
         text: generated_link
     }
     //to be updated in databse
     await ticket.findByIdAndUpdate(Ticket._id, { url: genString }, (err) => { console.log });
     // console.log(generated_link)
-    //MAILING VIA MAILGUN
-    // await mail.messages().send(data, (error, body) => {
-    //     console.log(error, body)
-    // })
-    //MAILING VIA SENDGR
+
     try {
-        await mail.send(data);
+        await sendgrid.send(data);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         return false;
     }
-
     return true;
 }
 
