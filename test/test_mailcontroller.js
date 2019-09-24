bd768106f197b1f9bbf46dd2b6f9cbf61f65bbf4
Neil Agarwal <neil@neilagarwal.me>
2019-09-24 20:50:03 +0530

    perfest-backend: Add test folder to .gitignore

--- test/test_mailcontroller.js
+++
@@ -1,9 +0,0 @@
-var { resetPassword } = require('../app/controllers/mailController');
-var expect = require('chai').expect;
-describe("#resetPassword",()=>{
-    context('with null object',()=>{
-        it('should return true',()=>{
-            expect(resetPassword(null)).to.equal(true);
-        })
-    })
-})
