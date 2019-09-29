c8f17fa41d55006560493342aea22c3c7bb8cc40
Neil Agarwal <neil@neilagarwal.me>
2019-09-29 18:00:59 +0530

    mailController: Use sendgrid dynamic transactional templates

--- app/controllers/mailController.js
+++ app/controllers/mailController.js
@@ -1,6 +1,5 @@
 const User = require('../database/models/user'),
     Ticket = require('../database/models/ticket'),
-    jwt = require('jsonwebtoken'),
     sendgrid = require('@sendgrid/mail');
 
 
@@ -9,27 +8,41 @@ require('dotenv').config();
 //SENDGRID CONFIGURATION
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-//add controllers below
 exports.eventConfirmation = async (user, ticket) => {
-    let userId = user._id;
     let userEmail = user.contact.email;
     let userName = user.name;
     let genString = Math.random().toString(36).substring(5);
-    let generated_link = process.env.HOST + "/t/" + genString;
+    let generated_link = 'https://' + process.env.HOST + "/t/" + genString;
+    let balanceBoolean = false;
+
     ticket = await Ticket.findById(ticket._id)
-            .populate('event')
-            .populate('volunteer_id.value')
-    var data = {
-        from: 'Perfest <tickets@perfest.co>',
-        to: userEmail,
-        subject: 'Your Tickets',
-        text: generated_link
+        .populate('event')
+        .populate('volunteer_id.value')
+
+    if (ticket.balance > 0 || ticket.balance !== null) {
+        balanceBoolean = true;
     }
-    //to be updated in databse
+
+    const mailData = {
+        to: userEmail,
+        from: 'Perfest <tickets@perfest.co>',
+        templateId: 'd-a0a73b06fd99493889935a30fe0ece28',
+        dynamic_template_data: {
+            "firstName": userName,
+            "event": ticket.event.name,
+            "fest": "Techmate 2019",
+            "venue": "RAIT",
+            "date": "10th October",
+            "balanceBoolean": balanceBoolean,
+            "balance": ticket.balance,
+            "ticketLink": generated_link
+        }
+    }
+
+    // Update in database
     await Ticket.findByIdAndUpdate(ticket._id, { url: genString }, (err) => { console.log });
-    // console.log(generated_link)
     try {
-        await sendgrid.send(data);
+        await sendgrid.send(mailData);
     }
     catch (err) {
         console.log(err);
@@ -39,10 +52,8 @@ exports.eventConfirmation = async (user, ticket) => {
 }
 
 exports.resetPassword = async (user) => {
-    // let userEmail = "kolisomesh27@gmail.com";
     let userEmail = user.contact.email;
     let userName = user.name;
-    // let userName = "Somesh koli";
     let genString = Math.random().toString(36).substring(3);
     let generated_link = process.env.HOST + "/c/" + genString;
     var data = {
@@ -52,14 +63,10 @@ exports.resetPassword = async (user) => {
         text: "Dear " + userName + ",\n Use this email to reset your password.The link will be active for 30 mintue from the issue time.\n"
             + generated_link
     }
-    //udpdate reset url  into database
+    // Update the reset url in database
     await User.findByIdAndUpdate(user._id, { url: genString }, (err) => { console.log });
 
-    //MAILING VIA MAILGUN
-    // await mail.messages().send(data, (error, body) => {
-    //     console.log(error, body)
-    // })
-    //MAILING VIA SENDGR
+    // Send mail via sendgrid
     try {
         await sendgrid.send(data);
     }
